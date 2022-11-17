9bcc1057fcbdd6525d4b4b9222dd6b3079f81f60
Andrii <shandrij1@gmail.com>
2022-11-17 12:00:17 +0100

    copy accelerometers

--- index-2.php
+++ index-2.php
@@ -3,50 +3,44 @@
 <head>
 
 <title>Accelerometer</title>
-<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+
+
 <link rel="stylesheet" href="font/ss-standard.css" />
-<link rel="stylesheet" href="style-1.css" />
+<link rel="stylesheet" href="style-2.css" />
 <link rel="stylesheet" href="../gs.css" />
 
-<script src='jquery.js'></script>
-<script src='javascript-1.js'></script>
+<meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
 
+<script src='jquery.js'></script>
+<script src='javascript-2.js'></script>
+
+<style type="text/css">
+
+/* ==================== IGNORE */
+
+
+</style>
 </head>
 <body>
 
-<div id="container">
-	<div class='box'>
+<div class='x'></div>
+
+<div id='container'>
 	
-	</div>
+	<div class='left-edge'></div>
+	<div class='right-edge'></div>
+	<div class='top-edge'></div>
+	<div class='bottom-edge'></div>
+	<div class='bottom-box'></div>
 	
-	<div class='box'>
-	
-	</div>
-	
-	<div class='box'>
-	
-	</div>
-	
-	<div class='box'>
-	
-	</div>
-	
-	<div class='box'>
-	
-	</div>
-	
-	<div class='box'>
-	
-	</div>
 </div>
 
 <div class='content tk-museo-sans'>
 	<h1>Hey! This Only Works on Mobiles</h1>
 	<p>And you're not on a mobile device! Don't worry though, hover over the box on the left to get a vibe for what this effect looks like. The only difference is this would be accomplished on mobiles by moving the device. (i.e. using the accelerometer)</p>
-	<a class='c' href="index-1.php">Example 1</a>
-	<a class='ex' href="index-2.php">Example 2</a>
+	<a class='ex' href="index-1.php">Example 1</a>
+	<a class='c' href="index-2.php">Example 2</a>
 </div>
-
 
 </body>
 </html>
